b942ea81bed76b0f0ec2841a2a2cfcc9e08bdab1
landov <egylabon@yahoo.co.uk>
2022-05-12 13:00:25 +0200

    logging

--- src/main/java/hu/landov/mnb/rest/MnbRestApplication.java
+++ src/main/java/hu/landov/mnb/rest/MnbRestApplication.java
@@ -2,12 +2,24 @@ package hu.landov.mnb.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class MnbRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MnbRestApplication.class, args);
+	}
+
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+		CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+		loggingFilter.setIncludeClientInfo(true);
+		loggingFilter.setIncludeQueryString(true);
+		loggingFilter.setIncludePayload(true);
+		loggingFilter.setIncludeHeaders(false);
+		return loggingFilter;
 	}
 
 }
